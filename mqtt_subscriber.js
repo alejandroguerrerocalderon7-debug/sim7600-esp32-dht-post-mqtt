a109fb55e726e0b770550bd45ad293c9342b318b
alejandroguerrerocalderon7-debug <alejandroguerrerocalderon7@gmail.com>
2025-12-02 09:27:21 +0100

    mqtt_subscriber.js

--- mqtt_subscriber.js
+++ mqtt_subscriber.js
@@ -1,23 +1,13 @@
  {
-... // Archivo: mqtt_subscriber.js
-... 
-... // 1. Importa la librería MQTT
+
 ... const mqtt = require('mqtt');
-... 
-... // 2. Configuración del Broker
-... // Usamos el mismo broker y puerto que el ESP32
+
 ... const brokerUrl = 'mqtt://broker.hivemq.com:1883'; 
-... 
-... // 3. Tópico al que el ESP32 está publicando
 ... const topic = 'ESP32_SIM7600_SENSOR_DATA';
-... 
-... // 4. Conectar al Broker
 ... const client = mqtt.connect(brokerUrl);
 ... 
 ... client.on('connect', () => {
 ...     console.log('✅ Conectado al broker MQTT: ' + brokerUrl);
-...     
-...     // 5. Suscribirse al tópico para empezar a recibir mensajes
 ...     client.subscribe(topic, (err) => {
 ...         if (!err) {
 ...             console.log(`✅ Suscrito al tópico: ${topic}. Esperando datos...`);
@@ -32,7 +22,6 @@
 ... });
 ... 
 ... client.on('message', (receivedTopic, message) => {
-...     // 6. Al recibir un mensaje, lo mostramos en la consola
 ...     if (receivedTopic === topic) {
 ...         try {
 ...             const payload = JSON.parse(message.toString());
@@ -48,7 +37,6 @@
         }
     }
 });
-
-// Manejo de desconexión
 client.on('close', () => {
     console.log('⚠️ Desconectado del servidor.');
+
